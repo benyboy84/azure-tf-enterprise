c95bdf8bacae4efffa08cb114ee4aeaf7348d577
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-05-11 14:27:50 -0400

    ft: Management Groups

--- locals.tf
+++ locals.tf
@@ -4,3 +4,16 @@ locals {
   root_name      = var.root_name
   root_parent_id = var.root_parent_id
 }
+
+# The following locals are used to define base Azure
+# provider paths and resource types
+locals {
+  provider_path = {
+    management_groups = "/providers/Microsoft.Management/managementGroups/"
+    role_assignment   = "/providers/Microsoft.Authorization/roleAssignments/"
+  }
+  resource_types = {
+    policy_definition     = "Microsoft.Authorization/policyDefinitions"
+    policy_set_definition = "Microsoft.Authorization/policySetDefinitions"
+  }
+}

aec0c76c6d499b1cba9c8db9af114fd889416853
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-05-11 14:32:10 -0400

    ft: Management Groups

--- locals.tf
+++ locals.tf
@@ -1,3 +1,10 @@
+# The following block of locals are used to avoid using empty object types in the code
+locals {
+  empty_list   = []
+  empty_map    = tomap({})
+  empty_string = ""
+}
+
 # The following locals are used to convert provided input variables to locals before use elsewhere in the module
 locals {
   root_id        = var.root_id

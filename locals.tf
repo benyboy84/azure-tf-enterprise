96790904aa6601415063de6db641f70ff4ade232
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-05-16 14:36:01 -0400

    ft: Management Groups

--- locals.tf
+++ locals.tf
@@ -7,10 +7,10 @@ locals {
 
 # The following locals are used to convert provided input variables to locals before use elsewhere in the module
 locals {
-  root_id              = var.root_id
-  root_name            = var.root_name
-  root_parent_id       = var.root_parent_id
-  landing_zones = var.landing_zones
+  root_id        = var.root_id
+  root_name      = var.root_name
+  root_parent_id = var.root_parent_id
+  landing_zones  = var.landing_zones
 }
 
 # The following locals are used to define base Azure
